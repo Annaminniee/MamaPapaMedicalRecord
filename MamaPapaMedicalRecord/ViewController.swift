cba172b44ea1c403e18a17190343e8182ad098fa
Anna <anna.kikuchihara@gmail.com>
2023-09-08 21:28:15 +0900

    Initial Commit

---
+++ MamaPapaMedicalRecord/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MamaPapaMedicalRecord
+//
+//  Created by 菊地原杏菜 on 2023/09/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
