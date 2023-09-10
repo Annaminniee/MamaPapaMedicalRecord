3df8493ee2d7b79f26a524a792b97ee944ac983e
Anna <anna.kikuchihara@gmail.com>
2023-09-10 09:54:28 +0900

    コードフォーマットをかけました

--- MamaPapaMedicalRecord/CoverViewController.swift
+++ MamaPapaMedicalRecord/CoverViewController.swift
@@ -17,17 +17,17 @@ final class CoverViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "名前"
-            
+        
         let calendarBarButtonItem = UIBarButtonItem(image: UIImage(named: "calendarIcon"),
                                                     style: UIBarButtonItem.Style.plain,
                                                     target: self,
                                                     action: #selector(tapCalendar))
         self.navigationItem.leftBarButtonItem = calendarBarButtonItem
-            
+        
         let familyBarButtonItem = UIBarButtonItem(image: UIImage(named: "familySettingIcon"),
-                                                    style: UIBarButtonItem.Style.plain,
-                                                    target: self,
-                                                    action: #selector(tapFamily))
+                                                  style: UIBarButtonItem.Style.plain,
+                                                  target: self,
+                                                  action: #selector(tapFamily))
         self.navigationItem.rightBarButtonItem = familyBarButtonItem
         
         let monthLabel = UILabel()
@@ -45,13 +45,13 @@ final class CoverViewController: UIViewController {
         todayLabel.text = ""
         self.view.addSubview(todayLabel)
         
-        }
-        
+    }
+    
     @objc func tapCalendar() {
         print("カレンダーアイコンがタップされました")
         ///画面遷移処理
     }
-        
+    
     @objc func tapFamily() {
         print("家族アイコンがタップされました")
         ///画面遷移処理
