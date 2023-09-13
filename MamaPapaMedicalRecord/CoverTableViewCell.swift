197cc68c8625d9b8f7bcb5b0395a6b2c11bd1d1a
Anna <anna.kikuchihara@gmail.com>
2023-09-13 17:03:21 +0900

    カスタムTableViewCellの実装をしました

---
+++ MamaPapaMedicalRecord/CoverTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  CoverTableViewCell.swift
+//  MamaPapaMedicalRecord
+//
+//  Created by 菊地原杏菜 on 2023/09/13.
+//
+
+import UIKit
+
+class CoverTableViewCell: UITableViewCell {
+
+    @IBOutlet private weak var memoLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    private func setup(memo: String) {
+        memoLabel.text = memo
+    }
+    
+}
