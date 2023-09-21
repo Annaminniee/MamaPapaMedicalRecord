9c98e1bf72a01db047f2a0f5ff597013574da2c1
Anna <anna.kikuchihara@gmail.com>
2023-09-21 15:16:25 +0900

    記録ノート一覧画面のUIパーツの配置とIBOutlet接続と表紙画面からの遷移を実装しました

--- MamaPapaMedicalRecord/RecordNotesListViewController.swift
+++ MamaPapaMedicalRecord/RecordNotesListViewController.swift
@@ -10,8 +10,32 @@ import UIKit
 /// 記録ノート一覧画面
 final class RecordNotesListViewController: UIViewController {
     
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - View Life-Cycle Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureCancelButtonItem()
+        navigationItem.title = "記録ノート一覧"
+    }
+    
+    // MARK: - Other Methods
+    
+    /// 戻るボタンの設定
+    private func configureCancelButtonItem() {
+        let cancelButton = UIBarButtonItem(title: "<戻る",
+                                           style: .plain,
+                                           target: self,
+                                           action: #selector(cancelButtonTapped))
+        navigationItem.leftBarButtonItem = cancelButton
+    }
+    
+    @objc func cancelButtonTapped() {
+        // 前の画面に戻る
+        navigationController?.popViewController(animated: true)
     }
 }
 
