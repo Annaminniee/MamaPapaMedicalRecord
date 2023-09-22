e4c6579ca60945126269ca5ea388106f67129f12
Anna Kikuchihara <anna.kikuchihara@gmail.com>
2023-09-22 16:51:10 +0900

    記録ノート一覧画面のUIパーツの配置とIBOutlet接続を実装しました。

--- MamaPapaMedicalRecord/RecordNotesListViewController.swift
+++ MamaPapaMedicalRecord/RecordNotesListViewController.swift
@@ -10,8 +10,18 @@ import UIKit
 /// 記録ノート一覧画面
 final class RecordNotesListViewController: UIViewController {
     
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - View Life-Cycle Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.title = "記録ノート一覧"
     }
+    
+    // MARK: - Other Methods
+    
 }
 
