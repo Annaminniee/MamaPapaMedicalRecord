c64c6f6a9a30cafd9ee33aef1882263f10d5e2bc
Anna Kikuchihara <anna.kikuchihara@gmail.com>
2023-09-23 09:52:18 +0900

    IBOutletで各種ボタンとテキストフィールドを繋ぎました

--- MamaPapaMedicalRecord/MemoViewController.swift
+++ MamaPapaMedicalRecord/MemoViewController.swift
@@ -12,6 +12,34 @@ final class MemoViewController: UIViewController {
     
     // MARK: - IBOutlets
     
+    @IBOutlet private weak var dateTextField: UITextField!
+    @IBOutlet private weak var recordNameTextField: UITextField!
+    @IBOutlet private weak var feverButton: CustomButton!
+    @IBOutlet private weak var diarrheaButton: CustomButton!
+    @IBOutlet private weak var vomitingButton: CustomButton!
+    @IBOutlet private weak var convulsionsButton: CustomButton!
+    @IBOutlet private weak var rashButton: CustomButton!
+    @IBOutlet private weak var otherButton: CustomButton!
+    @IBOutlet private weak var appetiteNormalButton: CustomButton!
+    @IBOutlet private weak var appetiteLittleButton: CustomButton!
+    @IBOutlet private weak var appetiteNoButton: CustomButton!
+    @IBOutlet private weak var hydrationYesButton: CustomButton!
+    @IBOutlet private weak var hydrationLittleButton: CustomButton!
+    @IBOutlet private weak var hydrationNoButton: CustomButton!
+    @IBOutlet private weak var moodGoodButton: CustomButton!
+    @IBOutlet private weak var moodNormalButton: CustomButton!
+    @IBOutlet private weak var moodBadButton: CustomButton!
+    @IBOutlet private weak var moodTiredButton: CustomButton!
+    @IBOutlet private weak var sleepMuchButton: CustomButton!
+    @IBOutlet private weak var sleepNormalButton: CustomButton!
+    @IBOutlet private weak var sleepLittleButton: CustomButton!
+    @IBOutlet private weak var sleepNoButton: CustomButton!
+    @IBOutlet private weak var sleepHourTextField: UITextField!
+    @IBOutlet private weak var sleepMinuteButton: UITextField!
+    @IBOutlet private weak var coughYesButton: CustomButton!
+    @IBOutlet private weak var coughNoButton: CustomButton!
+    @IBOutlet private weak var snotYesButton: CustomButton!
+    @IBOutlet private weak var snotNoButton: CustomButton!
     @IBOutlet private weak var tableView: UITableView!
     
     // MARK: - View Life-Cycle Methods
