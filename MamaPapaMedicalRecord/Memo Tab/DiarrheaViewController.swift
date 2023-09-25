49d4ec3860652c9e759f0f8f74bd5ed0df415fa9
Anna Kikuchihara <anna.kikuchihara@gmail.com>
2023-09-25 14:31:35 +0900

    フォルダーを作りました。

---
+++ MamaPapaMedicalRecord/Memo Tab/DiarrheaViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DiarrheaViewController.swift
+//  MamaPapaMedicalRecord
+//
+//  Created by 菊地原杏菜 on 2023/09/25.
+//
+
+import UIKit
+
+class DiarrheaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
