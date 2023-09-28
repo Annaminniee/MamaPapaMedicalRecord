003a0a79ab6bf3f4212bbc75b275df34bb5af3ae
Anna Kikuchihara <anna.kikuchihara@gmail.com>
2023-09-28 10:05:44 +0900

    ibactionのスペースおよび名前を記録しました

--- MamaPapaMedicalRecord/Memo Tab/MemoViewController.swift
+++ MamaPapaMedicalRecord/Memo Tab/MemoViewController.swift
@@ -29,64 +29,117 @@ final class MemoViewController: UIViewController {
     
     // MARK: - IBActions
     
-    @IBAction func feverButtong(_ sender: CustomButton) {
+    /// 熱ボタン
+    @IBAction func feverButton(_ sender: CustomButton) {
     }
+    
+    /// 下痢ボタン
     @IBAction func diarrheaButton(_ sender: CustomButton) {
     }
+    
+    /// 嘔吐ボタン
     @IBAction func vomitingButton(_ sender: CustomButton) {
     }
+    
+    /// 痙攣ボタン
     @IBAction func convulsionButton(_ sender: CustomButton) {
     }
+    
+    /// 発疹ボタン
     @IBAction func rashButton(_ sender: CustomButton) {
     }
+    
+    /// その他ボタン
     @IBAction func otherButton(_ sender: CustomButton) {
     }
+    
+    /// 食欲普通ボタン
     @IBAction func appetiteNormal(_ sender: CustomButton) {
     }
+    
+    /// 食欲少しボタン
     @IBAction func appetiteLittleButton(_ sender: CustomButton) {
     }
+    
+    /// 食欲なしボタン
     @IBAction func appetiteNoButton(_ sender: CustomButton) {
     }
+    
+    /// 水分補給できたボタン
     @IBAction func hydrationYesButton(_ sender: CustomButton) {
     }
+    
+    /// 水分補給少しボタン
     @IBAction func hydrationLittleButton(_ sender: CustomButton) {
     }
+    
+    /// 水分補給できないボタン
     @IBAction func hydrationNoButton(_ sender: CustomButton) {
     }
+    
+    /// 機嫌良いボタン
     @IBAction func moodGoodButton(_ sender: CustomButton) {
     }
+    
+    /// 機嫌普通ボタン
     @IBAction func moodNormalButton(_ sender: CustomButton) {
     }
+    
+    /// 機嫌悪いボタン
     @IBAction func moodBadButton(_ sender: CustomButton) {
     }
+    
+    /// 機嫌ぐったりボタン
     @IBAction func moodTiredButton(_ sender: CustomButton) {
     }
+    
+    /// 睡眠多いボタン
     @IBAction func sleepMuchButton(_ sender: CustomButton) {
     }
+    
+    /// 睡眠普通ボタン
     @IBAction func sleepNormalButton(_ sender: CustomButton) {
     }
+    
+    /// 睡眠少ないボタン
     @IBAction func sleepLittleButton(_ sender: CustomButton) {
     }
+    
+    /// 睡眠寝てないボタン
     @IBAction func sleepNoButton(_ sender: CustomButton) {
     }
+    
+    /// 咳ありボタン
     @IBAction func coughYesButton(_ sender: CustomButton) {
     }
+    
+    /// 咳なしボタン
     @IBAction func coughNoButton(_ sender: CustomButton) {
     }
+    
+    /// 鼻水ありボタン
     @IBAction func snotYesButton(_ sender: CustomButton) {
     }
+    
+    /// 鼻水なしボタン
     @IBAction func snotNoButton(_ sender: CustomButton) {
     }
+    
+    /// 日にち設定
     @IBAction func changeDate(_ sender: UIDatePicker) {
         let formatter = DateFormatter()
                 formatter.dateFormat = "yyyy/MM/dd"
         dateTextField.text = formatter.string(from: sender.date)
     }
+    
+    /// 睡眠時間（時）設定
     @IBAction func sleepHour(_ sender: UIDatePicker) {
         let formatter = DateFormatter()
                 formatter.dateFormat = "HH:hh"
         sleepHourTextField.text = formatter.string(from: sender.date)
     }
+    
+    /// 睡眠時間（分）設定
     @IBAction func sleepMinute(_ sender: UIDatePicker) {
         let formatter = DateFormatter()
                 formatter.dateFormat = "HH:mm"
