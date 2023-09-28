62399fbc09badd0ee39db266e9a7031d9747f6ac
Anna Kikuchihara <anna.kikuchihara@gmail.com>
2023-09-28 13:52:39 +0900

    【メモ画面作成_嘔吐】UIパーツの配置とIBOutlet接続とメモ画面からの遷移を実装しました。

--- MamaPapaMedicalRecord/Memo Tab/VomitingViewController.swift
+++ MamaPapaMedicalRecord/Memo Tab/VomitingViewController.swift
@@ -8,22 +8,48 @@
 import UIKit
 
 class VomitingViewController: UIViewController {
-
+    
+    // MARK: - IBOutlets
+    
+    
+    // MARK: - View Life-Cycle Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureSaveButtonItem()
+        navigationItem.title = "嘔吐"
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    // MARK: - IBActions
+    
+    
+    
+    // MARK: - Other Methods
+    
+    /// 戻るボタンの設定
+    private func configureCancelButtonItem() {
+        let cancelButton = UIBarButtonItem(title: "<戻る",
+                                           style: .plain,
+                                           target: self,
+                                           action: #selector(cancelButtonTapped))
+        navigationItem.leftBarButtonItem = cancelButton
     }
-    */
-
+    
+    @objc func cancelButtonTapped() {
+        // 前の画面に戻る
+        navigationController?.popViewController(animated: true)
+    }
+    
+    private func configureSaveButtonItem() {
+        let saveButton = UIBarButtonItem(title: "登録",
+                                         style: .plain,
+                                         target: self,
+                                         action: #selector(saveButtonTapped))
+        navigationItem.rightBarButtonItem = saveButton
+    }
+    
+    @objc func saveButtonTapped() {
+        // 保存処理
+    }
 }
+
