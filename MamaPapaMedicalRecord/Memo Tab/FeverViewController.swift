2870a93af30cb98095264cde1340b455e19ade36
Anna Kikuchihara <anna.kikuchihara@gmail.com>
2023-09-28 09:42:52 +0900

    【メモ画面作成_熱】UIパーツの配置とIBOutlet接続とメモ画面からの遷移を実装しました。

--- MamaPapaMedicalRecord/Memo Tab/FeverViewController.swift
+++ MamaPapaMedicalRecord/Memo Tab/FeverViewController.swift
@@ -8,22 +8,56 @@
 import UIKit
 
 class FeverViewController: UIViewController {
-
+    
+    // MARK: - IBOutlets
+    @IBOutlet private weak var recordDateTextField: UITextField!
+    @IBOutlet private weak var temperatureTextField: UITextField!
+    @IBOutlet private weak var memoTableView: UITableView!
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var photoImageView: UIImageView!
+    @IBOutlet private weak var cameraImageView: UIImageView!
+    @IBOutlet private weak var trashImageView: UIImageView!
+    
+    // MARK: - View Life-Cycle Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureSaveButtonItem()
+        navigationItem.title = "熱"
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    // MARK: - IBActions
+    
+    @IBAction func antipyreticAgentYesButton(_ sender: CustomButton) {
     }
-    */
-
+    @IBAction func antipyreticAgentNoButton(_ sender: CustomButton) {
+    }
+    
+    // MARK: - Other Methods
+    
+    /// 戻るボタンの設定
+    private func configureCancelButtonItem() {
+        let cancelButton = UIBarButtonItem(title: "<戻る",
+                                           style: .plain,
+                                           target: self,
+                                           action: #selector(cancelButtonTapped))
+        navigationItem.leftBarButtonItem = cancelButton
+    }
+    
+    @objc func cancelButtonTapped() {
+        // 前の画面に戻る
+        navigationController?.popViewController(animated: true)
+    }
+    
+    private func configureSaveButtonItem() {
+        let saveButton = UIBarButtonItem(title: "登録",
+                                         style: .plain,
+                                         target: self,
+                                         action: #selector(saveButtonTapped))
+        navigationItem.rightBarButtonItem = saveButton
+    }
+    
+    @objc func saveButtonTapped() {
+        // 保存処理
+    }
 }
