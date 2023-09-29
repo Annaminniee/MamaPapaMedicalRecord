e890eaccf99570432d7383d6755282d1fb9ab54d
Anna Kikuchihara <anna.kikuchihara@gmail.com>
2023-09-29 14:19:37 +0900

    コードフォーマットをかけました

--- MamaPapaMedicalRecord/Memo Tab/DiarrheaViewController.swift
+++ MamaPapaMedicalRecord/Memo Tab/DiarrheaViewController.swift
@@ -8,48 +8,48 @@
 import UIKit
 
 class DiarrheaViewController: UIViewController {
-
-        // MARK: - IBOutlets
-
-        
-        // MARK: - View Life-Cycle Methods
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            configureSaveButtonItem()
-            navigationItem.title = "痙攣"
-        }
-        
-        // MARK: - IBActions
-        
-
-        
-        // MARK: - Other Methods
-        
-        /// 戻るボタンの設定
-        private func configureCancelButtonItem() {
-            let cancelButton = UIBarButtonItem(title: "<戻る",
-                                               style: .plain,
-                                               target: self,
-                                               action: #selector(cancelButtonTapped))
-            navigationItem.leftBarButtonItem = cancelButton
-        }
-        
-        @objc func cancelButtonTapped() {
-            // 前の画面に戻る
-            navigationController?.popViewController(animated: true)
-        }
-        
-        private func configureSaveButtonItem() {
-            let saveButton = UIBarButtonItem(title: "登録",
-                                             style: .plain,
-                                             target: self,
-                                             action: #selector(saveButtonTapped))
-            navigationItem.rightBarButtonItem = saveButton
-        }
-        
-        @objc func saveButtonTapped() {
-            // 保存処理
-        }
+    
+    // MARK: - IBOutlets
+    
+    
+    // MARK: - View Life-Cycle Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureSaveButtonItem()
+        navigationItem.title = "痙攣"
     }
+    
+    // MARK: - IBActions
+    
+    
+    
+    // MARK: - Other Methods
+    
+    /// 戻るボタンの設定
+    private func configureCancelButtonItem() {
+        let cancelButton = UIBarButtonItem(title: "<戻る",
+                                           style: .plain,
+                                           target: self,
+                                           action: #selector(cancelButtonTapped))
+        navigationItem.leftBarButtonItem = cancelButton
+    }
+    
+    @objc func cancelButtonTapped() {
+        // 前の画面に戻る
+        navigationController?.popViewController(animated: true)
+    }
+    
+    private func configureSaveButtonItem() {
+        let saveButton = UIBarButtonItem(title: "登録",
+                                         style: .plain,
+                                         target: self,
+                                         action: #selector(saveButtonTapped))
+        navigationItem.rightBarButtonItem = saveButton
+    }
+    
+    @objc func saveButtonTapped() {
+        // 保存処理
+    }
+}
 
