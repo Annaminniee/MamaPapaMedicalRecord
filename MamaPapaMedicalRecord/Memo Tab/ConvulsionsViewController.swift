e48accf50051a9e24798643f5172a455404a4dfb
Anna Kikuchihara <anna.kikuchihara@gmail.com>
2023-09-29 15:46:03 +0900

    【メモ画面作成_下痢】UIパーツの配置とIBOutlet接続とメモ画面からの遷移を実装しました。

--- MamaPapaMedicalRecord/Memo Tab/ConvulsionsViewController.swift
+++ MamaPapaMedicalRecord/Memo Tab/ConvulsionsViewController.swift
@@ -7,23 +7,80 @@
 
 import UIKit
 
-class ConvulsionsViewController: UIViewController {
+final class ConvulsionsViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    
+    /// 記録日時設定
+    @IBOutlet private weak var recordDateTextField: UITextField!
+    
+    /// 体温設定
+    @IBOutlet private weak var temperatureTextField: UITextField!
+    
+    /// 色その他
+    @IBOutlet private weak var colorOtherTextField: UITextField!
+    
+    /// におい
+    @IBOutlet private weak var smellTextField: UITextField!
+    
+    /// メモ設定
+    @IBOutlet private weak var memoTableView: UITableView!
+    
+    /// 画像設定
+    @IBOutlet private weak var imageView: UIImageView!
+    
+    /// 画像挿入設定
+    @IBOutlet private weak var photoImageView: UIImageView!
+    
+    /// カメラ・動画挿入設定
+    @IBOutlet private weak var cameraImageView: UIImageView!
+    
+    /// 削除設定
+    @IBOutlet private weak var trashImageView: UIImageView!
+    
+    // MARK: - View Life-Cycle Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureSaveButtonItem()
+        navigationItem.title = "下痢"
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    // MARK: - IBActions
+    
+    /// 解熱剤使用はいボタン
+    @IBAction private func antipyreticAgentYesButton(_ sender: CustomButton) {
     }
-    */
-
+    
+    /// 解熱剤使用なしボタン
+    @IBAction private func antipyreticAgentNoButton(_ sender: CustomButton) {
+    }
+    
+    // MARK: - Other Methods
+    
+    /// 戻るボタンの設定
+    private func configureCancelButtonItem() {
+        let cancelButton = UIBarButtonItem(title: "<戻る",
+                                           style: .plain,
+                                           target: self,
+                                           action: #selector(cancelButtonTapped))
+        navigationItem.leftBarButtonItem = cancelButton
+    }
+    
+    @objc func cancelButtonTapped() {
+        // 前の画面に戻る
+        navigationController?.popViewController(animated: true)
+    }
+    
+    private func configureSaveButtonItem() {
+        let saveButton = UIBarButtonItem(title: "登録",
+                                         style: .plain,
+                                         target: self,
+                                         action: #selector(saveButtonTapped))
+        navigationItem.rightBarButtonItem = saveButton
+    }
+    
+    @objc func saveButtonTapped() {
+        // 保存処理
+    }
 }
