3cee59e8823c367a1ecb7d3a56f253a21a70dfa4
Anna Kikuchihara <anna.kikuchihara@gmail.com>
2023-10-02 11:03:58 +0900

    体裁を整え、ビルドした確認しました

--- MamaPapaMedicalRecord/Memo Tab/DiarrheaViewController.swift
+++ MamaPapaMedicalRecord/Memo Tab/DiarrheaViewController.swift
@@ -14,54 +14,49 @@ final class DiarrheaViewController: UIViewController {
     
     /// 記録日時
     @IBOutlet private weak var dateTextField: UITextField!
-    
     /// 痙攣（分）
     @IBOutlet private weak var diarrheaMinuteTextField: UITextField!
-    
     /// 痙攣（時間）
     @IBOutlet private weak var diarrheaSecondTextField: UITextField!
-    
     /// 体温
     @IBOutlet private weak var temperatureTextField: UITextField!
-    
     /// メモ
-    @IBOutlet private weak var memoTableView: UITableView!
-    
+    @IBOutlet private weak var textView: UITextView!
     /// 画像挿入
     @IBOutlet private weak var imageView: UIImageView!
-    
-    /// 写真挿入
-    @IBOutlet private weak var photoImageView: UIImageView!
-    
-    /// カメラ・動画挿入
-    @IBOutlet private weak var cameraImageView: UIImageView!
-    
-    /// 削除
-    @IBOutlet private weak var trashImageView: UIImageView!
     
     // MARK: - View Life-Cycle Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureCancelButtonItem()
         configureSaveButtonItem()
         navigationItem.title = "痙攣"
     }
     
     // MARK: - IBActions
     
-    /// 意識はいボタン
-    @IBAction private func consciousnessYesButton(_ sender: CustomButton) {
+    /// 意識「はい」ボタン
+    @IBAction private func tapConsciousnessYesButton(_ sender: CustomButton) {
     }
-    
-    /// 意識いいえボタン
-    @IBAction private func consciousnessNoButton(_ sender: CustomButton) {
+    /// 意識「いいえ」ボタン
+    @IBAction private func tapConsciousnessNoButton(_ sender: CustomButton) {
+    }
+    /// 写真挿入ボタン
+    @IBAction private func tapPhotoButton(_ sender: UIButton) {
+    }
+    /// カメラ・動画起動ボタン
+    @IBAction private func tapCameraButton(_ sender: UIButton) {
+    }
+    /// 削除ボタン
+    @IBAction private func tapTrashButton(_ sender: UIButton) {
     }
     
     // MARK: - Other Methods
     
     /// 戻るボタンの設定
     private func configureCancelButtonItem() {
-        let cancelButton = UIBarButtonItem(title: "<戻る",
+        let cancelButton = UIBarButtonItem(title: "閉じる",
                                            style: .plain,
                                            target: self,
                                            action: #selector(cancelButtonTapped))
