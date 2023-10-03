81ca21eb98fee390f162e837327bb6ad6578e4bd
Anna Kikuchihara <anna.kikuchihara@gmail.com>
2023-10-03 16:28:56 +0900

    【記録ノート詳細画面作成】UIパーツの配置とIBOutlet接続と前画面からの遷移を実装しました。

--- MamaPapaMedicalRecord/Record Notes List Tab/RecordDetailViewController.swift
+++ MamaPapaMedicalRecord/Record Notes List Tab/RecordDetailViewController.swift
@@ -7,23 +7,67 @@
 
 import UIKit
 
-class RecordDetailViewController: UIViewController {
-
+///  記録ノート詳細画面
+final class RecordDetailViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    
+    /// 記録対象者
+    @IBOutlet private weak var recordPersonTextField: UITextField!
+    /// 記録期間
+    @IBOutlet private weak var recordPeriodTextField: UITextField!
+    /// メモ一覧
+    @IBOutlet private weak var memoTableView: UITableView!
+    /// 受診記録
+    @IBOutlet weak var consultationTableView: UITableView!
+    
+    // MARK: - View Life-Cycle Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureCancelButtonItem()
+        configurePrintButtonItem()
+        navigationItem.title = ""
+        setupTapGestureRecognizer()
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    // MARK: - Other Methods
+    
+    /// 戻るボタンの設定
+    private func configureCancelButtonItem() {
+        let cancelButton = UIBarButtonItem(title: "<戻る",
+                                           style: .plain,
+                                           target: self,
+                                           action: #selector(backButtonTapped))
+        navigationItem.leftBarButtonItem = cancelButton
     }
-    */
-
+    
+    @objc func backButtonTapped() {
+        // 前の画面に戻る
+        navigationController?.popViewController(animated: true)
+    }
+    
+    private func configurePrintButtonItem() {
+        let printButton = UIBarButtonItem(image: UIImage(named: "printIcon"),
+                                          style: .plain,
+                                          target: self,
+                                          action: #selector(printButtonTapped))
+        navigationItem.rightBarButtonItem = printButton
+    }
+    
+    @objc func printButtonTapped() {
+        // TODO: プリント処理
+    }
+    
+    /// タップジェスチャーリコグナイザをセットアップ
+    private func setupTapGestureRecognizer() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    /// 画面のどこかをタップしてキーボードを閉じるメソッド
+    @objc private func handleTap() {
+        view.endEditing(true)
+    }
 }
+
